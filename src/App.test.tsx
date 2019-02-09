ad2602f63c04e0593c1ca1f40a22e93dea7e5fc2
Georgi Naumov <gonaumov@gmail.com>
2019-02-09 16:36:20 +0200

    Fix a test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from "redux";
+import supermarket from "./reducers/supermarket";
+import initialState from "./initialState";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = createStore(supermarket, initialState)
+  ReactDOM.render(<App store={store}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
