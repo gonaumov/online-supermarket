ee6b3819ac7fdcdbc4b14ee1c63f8f499307ccae
Georgi Naumov <gonaumov@gmail.com>
2019-02-11 03:34:07 +0200

    Fix a test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from 'redux';
 import supermarket from "./reducers/supermarket";
 import initialState from "./initialState";
+import thunk from 'redux-thunk'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const store = createStore(supermarket, initialState)
+  const store = createStore(supermarket, initialState,  applyMiddleware(thunk))
   ReactDOM.render(<App store={store}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
