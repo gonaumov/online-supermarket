0275b783d358a5f23a3490b8d91eef0c33be08df
Georgi Naumov <gonaumov@gmail.com>
2019-02-11 02:33:35 +0200

    Clean up initial state.

--- src/initialState.ts
+++ src/initialState.ts
@@ -1,40 +1,6 @@
 const initialState: State = {
-    categories: [{
-        id: '1',
-        selected: false,
-        name: 'First category',
-    },{
-        id: '2',
-        selected: false,
-        name: 'Second category',
-    }],
-    products: [{
-        categoryId: ['1'],
-        id: '1',
-        name: 'First category product one',
-        description: 'Description of First category product one',
-        selected: false
-    },{
-        categoryId: ['1'],
-        id: '2',
-        name: 'First category product two',
-        description: 'Description of First category product two',
-        selected: false
-    },
-    {
-        categoryId: ['2'],
-        id: '3',
-        name: 'Second category product one',
-        description: 'Description of second category product one',
-        selected: false
-    },
-    {
-        categoryId: ['2'],
-        id: '4',
-        name: 'Second category product two',
-        description: 'Description of second category product two',
-        selected: false
-    }],
+    categories: [],
+    products: [],
     search: null
 }
 
