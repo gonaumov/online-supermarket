fae9ddca0e87e2b5ba70b007c0f100e9ad827ed6
Georgi Naumov <gonaumov@gmail.com>
2019-02-23 17:47:12 +0200

    Deploy as a github page

---
+++ precache-manifest.82a21d5a79dc31feeb414f9c006e2a32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17db07f88ee301e886a4",
+    "url": "/online-supermarket/static/js/runtime~main.17db07f8.js"
+  },
+  {
+    "revision": "3e2ec43826e8ab3d23d0",
+    "url": "/online-supermarket/static/js/main.3e2ec438.chunk.js"
+  },
+  {
+    "revision": "66b30af754658def5d3d",
+    "url": "/online-supermarket/static/js/1.66b30af7.chunk.js"
+  },
+  {
+    "revision": "3e2ec43826e8ab3d23d0",
+    "url": "/online-supermarket/static/css/main.4fadb318.chunk.css"
+  },
+  {
+    "revision": "03db78dc20eb457473e599433635fff6",
+    "url": "/online-supermarket/index.html"
+  }
+];
